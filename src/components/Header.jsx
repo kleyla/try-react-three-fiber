47d7de9e6f02f867846e19650c743cb3f8952b09
kleyla <kleyla01@live.com>
2020-12-02 18:21:55 -0400

    some practice

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>REACT THREE FIVER APP</span>
+      </div>
+      <div className="episode">
+        <span>Karen</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
